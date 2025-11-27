1f8a9fa01f12f935b5da358f211d3f9fb525ab14
Ixecd <2192629378@qq.com>
2025-11-27 23:44:43 +0800

    common makefile

---
+++ makefiles/make-rules/common.mk
@@ -0,0 +1,9 @@
+# Copyright 2025 qc <2192629378@qq.com>. All Rights Reserved.
+# Use of this source code is governed by a MIT style
+# License that can be found in the LICENSE file.
+
+SHELL := /bin/bash
+
+# include the common make file
+COMMON_SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
