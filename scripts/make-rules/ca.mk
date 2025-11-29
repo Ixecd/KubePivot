316b846cf33dfeede9bc7c64485c216bee65a55c
Ixecd <135129249+Ixecd@users.noreply.github.com>
2025-11-30 03:27:14 +0800

    ca makefile

--- scripts/make-rules/ca.mk
+++ scripts/make-rules/ca.mk
@@ -10,7 +10,7 @@
 ca.gen.%:
 	$(eval CA := $(word 1, $(subst ., ,$*)))
 	@echo "===========> Generating CA files for $(CA)"
-	@${ROOT_DIR}/scripts/gencerts.sh generate-iam-cert $(OUTPUT_DIR)/cert $(CA)
+	@${ROOT_DIR}/scripts/gencerts.sh generate-cert $(OUTPUT_DIR)/cert $(CA)
 
 .PHONY: ca.gen
 ca.gen: $(addprefix ca.gen., $(CERTIFICATES))
